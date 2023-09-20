8470b50d6ca22cf58ba538fadc4e06bf622d73c2
yabpaseri <yab.82.hiro@gmail.com>
2023-09-20 22:50:34 +0900

    remove .env and add .vscode

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv {
-	readonly VITE_APP_TITLE: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
